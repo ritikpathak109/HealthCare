6abcd172cbe777ad016367b62c865b8cdc4cd73e
Ritik <ritikpathak109@gmail.com>
2025-06-13 22:05:18 +0530

    Updated frontend and backend

--- Frontend HealthCare/HealthCareFrontEnd/src/app/Modules/doctor/components/doctor-consultation/doctor-consultation.component.ts
+++ Frontend HealthCare/HealthCareFrontEnd/src/app/Modules/doctor/components/doctor-consultation/doctor-consultation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,10 +9,50 @@ import { Router } from '@angular/router';
 })
 export class DoctorConsultationComponent implements OnInit {
 
-  constructor(private router: Router) { }
-   ngOnInit() {
-    
-   }
+  constructor(private router: Router, private fb: FormBuilder) { }
+
+   ngOnInit() {}
+
+     consultationForm = this.fb.group({
+
+       Tests: this.fb.array([
+        this.fb.control(null)
+      ])
+        //  DoctorFirstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]*$'), Validators.minLength(3)]], 
+        //  DoctorLastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]*$'),  Validators.minLength(3)]],  
+        //  UserName: ['', [Validators.required, Validators.minLength(3)]],  
+        //  DoctorEmail: ['', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')]],  
+        //  UserPassword: ['', [Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')]],  
+        //  RoleId: ['', [Validators.required], ],
+        //  DoctorDateOfBirth: ['', [Validators.required]],
+        //  GenderId: ['', [Validators.required]],
+        //  CountryId: ['', [Validators.required]],
+        //  StateId: ['', [Validators.required]],
+        //  DoctorAddress: ['', [Validators.required,  Validators.minLength(5)]],
+        //  DoctorPhoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]], 
+        //  SpecializationId: ['', [Validators.required]],
+        //  ExperienceYears: ['', [Validators.required ]],
+       
+       });
+
+
+    addTest(): void {
+    (this.consultationForm.get('Tests') as FormArray).push(
+      this.fb.control(null)
+    );
+
+
+  }
+
+  removeTest(index:any) {
+    (this.consultationForm.get('Tests') as FormArray).removeAt(index);
+  }
+
+
+  getTestsFormControls(): AbstractControl[] {
+    return (<FormArray> this.consultationForm.get('Tests')).controls
+  }
+
 
    logout() {
     localStorage.clear();
